a247e960d9475ed733b7fab4bfa611af8c135c8d
Jared Graham <jaredgraham@mac-mini.mynetworksettings.com>
2024-11-15 11:30:42 -0500

    clerk test 2

---
+++ src/_middleware.ts
@@ -0,0 +1,72 @@
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// const isPublicRoute = createRouteMatcher([
+//   "/sign-in(.*)",
+//   "/sign-up(.*)",
+//   "/landing",
+//   "/pricing",
+//   "/",
+//   "/api/webhooks/clerk(.*)",
+//   "/api/webhooks/stripe(.*)",
+// ]);
+
+// export default clerkMiddleware(async (auth, request) => {
+//   if (!isPublicRoute(request)) {
+//     await auth.protect();
+//   }
+// });
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     // Always run for API routes
+//     "/(api|trpc)(.*)",
+//   ],
+// };
+
+// // import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// // const isProtectedRoute = createRouteMatcher(["/companies(.*)"]);
+
+// // export default clerkMiddleware((auth, req) => {
+// //   if (isProtectedRoute(req)) {
+// //     auth().protect();
+// //   }
+// // });
+
+// // export const config = {
+// //   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// // };
+
+// // import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// // import { NextResponse } from "next/server";
+
+// // // Define the public routes for the front-end
+// // const isPublicRoute = createRouteMatcher([
+// //   "/sign-in(.*)",
+// //   "/sign-up(.*)",
+// //   "/landing",
+// // ]);
+
+// // export default clerkMiddleware((auth, req) => {
+// //   // Only protect front-end routes (ignore API routes)
+// //   const currentRoute = new URL(req.url);
+
+// //   // Skip API routes
+// //   if (currentRoute.pathname.startsWith("/api/webhooks")) {
+// //     return NextResponse.next();
+// //   }
+
+// //   // Handle public front-end routes
+// //   if (isPublicRoute(req)) {
+// //     return NextResponse.next();
+// //   }
+
+// //   // Protect routes that are not public
+// //   const res = auth().protect();
+// // });
+
+// // export const config = {
+// //   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// // };
