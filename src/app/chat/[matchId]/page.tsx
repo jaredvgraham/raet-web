796b6358a7388ffe5f4eca67452ae024f0ff73f7
Jared Graham <jaredgraham@mac-mini.mynetworksettings.com>
2024-11-19 17:18:31 -0500

    some header stuff

--- src/app/chat/[matchId]/page.tsx
+++ src/app/chat/[matchId]/page.tsx
@@ -145,64 +145,66 @@ const ChatScreen = () => {
   }
 
   return (
-    <div className={`flex flex-col ${hideNav ? "h-screen" : "h-[95%]"}`}>
-      <Header
-        backArrow={true}
-        image={match.images[0]}
-        backDestination="/chat"
-        userName={match.name}
-        style="py-6 fixed top-0 w-full"
-        imageOnPress={() => setShowProfile(true)}
-      />
-      <div className="flex-grow overflow-y-auto p-4 bg-gray-100">
-        {messages.map((msg, index) => (
-          <div
-            key={index}
-            className={`flex ${
-              msg.senderId !== userId ? "justify-start" : "justify-end"
-            } mb-4`}
-          >
-            {msg.senderId !== userId && (
-              <div className="relative w-10 h-10 overflow-hidden rounded-full mr-1 self-end">
-                <Image
-                  src={match?.images?.[0]} // Fallback if no image
-                  alt={match?.name || "User Image"}
-                  width={40} // These ensure consistent dimensions
-                  height={40}
-                  className="object-cover w-full h-full"
-                />
-              </div>
-            )}
-            <div
-              className={`p-3 rounded-xl max-w-[80%]  ${
-                msg.senderId !== userId ? "bg-gray-200" : "bg-blue-100"
-              }`}
-            >
-              <p className="text-lg break-words  ">{msg.message}</p>
-            </div>
-          </div>
-        ))}
-        <div ref={messagesEndRef} />
-      </div>
-      <div className="flex items-center border-t border-gray-300 p-3 ">
-        <input
-          type="text"
-          value={message}
-          onFocus={() => setHideNav(true)}
-          onBlur={handleBlur}
-          onChange={(e) => setMessage(e.target.value)}
-          className="flex-grow border border-gray-400 p-3 rounded-full mr-3"
-          placeholder="Type a message"
+    <>
+      <div className={` flex flex-col ${hideNav ? "h-screen" : "h-[95%]"}`}>
+        <Header
+          backArrow={true}
+          image={match.images[0]}
+          backDestination="/chat"
+          userName={match.name}
+          style="py-6 fixed top-0 left-0  w-full z-50 "
+          imageOnPress={() => setShowProfile(true)}
         />
-        <button
-          onClick={sendMessage}
-          disabled={isSending}
-          className="px-4 py-2 bg-blue-500 text-white rounded-full"
-        >
-          Send
-        </button>
+        <div className="flex-grow overflow-y-auto p-4 bg-gray-100">
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={`flex ${
+                msg.senderId !== userId ? "justify-start" : "justify-end"
+              } mb-4`}
+            >
+              {msg.senderId !== userId && (
+                <div className="relative w-10 h-10 overflow-hidden rounded-full mr-1 self-end">
+                  <Image
+                    src={match?.images?.[0]} // Fallback if no image
+                    alt={match?.name || "User Image"}
+                    width={40} // These ensure consistent dimensions
+                    height={40}
+                    className="object-cover w-full h-full"
+                  />
+                </div>
+              )}
+              <div
+                className={`p-3 rounded-xl max-w-[80%]  ${
+                  msg.senderId !== userId ? "bg-gray-200" : "bg-blue-100"
+                }`}
+              >
+                <p className="text-lg break-words  ">{msg.message}</p>
+              </div>
+            </div>
+          ))}
+          <div ref={messagesEndRef} />
+        </div>
+        <div className="flex items-center border-t border-gray-300 p-3 ">
+          <input
+            type="text"
+            value={message}
+            onFocus={() => setHideNav(true)}
+            onBlur={handleBlur}
+            onChange={(e) => setMessage(e.target.value)}
+            className="flex-grow border border-gray-400 p-3 rounded-full mr-3"
+            placeholder="Type a message"
+          />
+          <button
+            onClick={sendMessage}
+            disabled={isSending}
+            className="px-4 py-2 bg-blue-500 text-white rounded-full"
+          >
+            Send
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
