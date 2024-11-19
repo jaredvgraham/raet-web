d98b33d7f1dfed47572800b75bfd899e5e214744
Jared Graham <jaredgraham@mac-mini.mynetworksettings.com>
2024-11-19 16:03:36 -0500

    no nav while typing

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React from "react";
+
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { motion } from "framer-motion";
 import { useShowNav } from "@/hooks/showNav";
@@ -7,6 +8,7 @@ import { useShowNav } from "@/hooks/showNav";
 const BottomNavigation = () => {
   const router = useRouter();
   const { showNav } = useShowNav();
+  const [keyboardVisible, setKeyboardVisible] = useState(false);
 
   // Define navigation items
   const navItems = [
@@ -16,18 +18,32 @@ const BottomNavigation = () => {
     { label: "Profile", icon: "/icons/profile.svg", route: "/profile" },
   ];
 
+  useEffect(() => {
+    const handleResize = () => {
+      // Compare the viewport height to a threshold to detect if the keyboard is open
+      const fullHeight = window.innerHeight;
+      const viewportHeight = document.documentElement.clientHeight;
+
+      setKeyboardVisible(viewportHeight < fullHeight * 0.8); // Adjust threshold as needed
+    };
+
+    // Add and clean up the resize listener
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (!showNav || keyboardVisible) {
+    return null; // Hide navigation bar
+  }
+
   return (
-    <div
-      className={`${
-        showNav && "fixed bottom-0 left-0 right-0"
-      }  bg-white border-t shadow-lg flex justify-around py-3 z-50`}
-    >
+    <div className="fixed bottom-0 left-0 right-0 bg-white border-t shadow-lg flex justify-around py-5 z-50">
       {navItems.map((item) => (
         <motion.button
           key={item.route}
           whileTap={{ scale: 0.9 }}
           onClick={() => router.push(item.route)}
-          className={`flex flex-col items-center `}
+          className="flex flex-col items-center"
         >
           <img
             src={item.icon}
